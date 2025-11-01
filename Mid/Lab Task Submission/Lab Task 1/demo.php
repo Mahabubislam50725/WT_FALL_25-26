10ac73e958db1d9eaf777c3e8ee68383cca6b4f2
Mahabub Islam <marufhasan7702@gmail.com>
2025-11-01 18:57:50 +0600

    Add files via upload
    Add Lab Task 1 solution (Mahabub_islam)

---
+++ Mid/Lab Task Submission/Lab Task 1/demo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Wellcome to My Website </title>
+</head>
+<body>
+  <h1>Hello world </h1>
+  <br></br>
+  <p>This is a paragragh</p>
+</body>
+</html>
