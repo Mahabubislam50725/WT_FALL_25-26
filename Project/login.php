3c6126d65e1d9a94bbaea79c271f9fdaec99cfd3
Mahabub Islam <marufhasan7702@gmail.com>
2025-12-12 23:32:43 +0600

    Added code inside Project folder

---
+++ Project/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Multi-Vendor E-Commerce Management System </title>
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="">
+    <h2>Login</h2>
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required><br><br>
+    
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br><br>
+    
+    <button type="submit">Login</button>
+</form>
+</body>
+</html>
